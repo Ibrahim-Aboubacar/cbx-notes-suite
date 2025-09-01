bb240d823343f912413b2d8970925cf0380983c2
Ibrahim Aboubacar <ibrahimaboubacarcisse@gmail.com>
2025-09-01 15:35:20 +0000

    feat: :sparkles: implement UI and basic functionality for Register

--- web-frontend/src/features/auth/services/authService.ts
+++ web-frontend/src/features/auth/services/authService.ts
@@ -39,6 +39,33 @@ export const AuthService = {
             });
     },
 
+    /**
+     * Register user
+     * @param username User username
+     * @param email User email
+     * @param password User password
+     * @returns Promise<TApiResponse<TAuthServiceResponse['TRegister']>>
+     * @throws Error if the request fails
+     * @example
+     * ```typescript
+     * const res = await AuthService.register("user.name", "email@example.com", "password");
+     * ```
+     */
+    register: async (username: string, email: string, password: string) => {
+        await Helper.sleep(2000);
+        return await api
+            .post<TApiResponse<TAuthServiceResponse["TRegister"]>>("api/v1/auth/register", { username, email, password })
+            .then((res) => {
+                if (res.data.status === 200) {
+                    return res.data.data;
+                }
+                throw new Error(res.data.message);
+            })
+            .catch((err) => {
+                throw err;
+            });
+    },
+
     // logout: async () => api.post("/auth/logout"),
 
     // getProfile: async () => api.get("/auth/me").then(res => res.data),
