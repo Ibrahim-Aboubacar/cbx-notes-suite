6241c669f7e47cfd530a2fa5ce42c5a705a5db7d
Ibrahim Aboubacar <ibrahimaboubacarcisse@gmail.com>
2025-09-03 00:40:17 +0000

    feat: 🎉 initial Spring Boot scaffolding

---
+++ backend-spring/src/test/java/com/conote/ConoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
