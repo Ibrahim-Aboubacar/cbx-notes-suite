35a726b608a94a8c7dd3118e8a7be7b7688029a1
Ibrahim Aboubacar <ibrahimaboubacarcisse@gmail.com>
2025-09-06 19:57:17 +0000

    (web) : updated env variable to use local host as backend api

--- mobile-app/env.ts
+++ mobile-app/env.ts
@@ -1,5 +1,4 @@
 export const env = {
-    // api_url: "http://127.0.0.1:8080",
+    api_url: "http://127.0.0.1:8080",
     // api_url: "http://192.168.100.6:8080/",
-    api_url: "http://10.107.107.233:8080/",
 }
